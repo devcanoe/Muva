d5eeaa3769cab565861cdf9d75e06033aef00d7f
maceteli <maceteligolden@gmail.com>
2023-05-31 19:15:27 +0100

    build: resolving issues with build

--- client/components/form/fileuploader/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.css';
-
-export default function FileUploader() {
-    return (
-        <>
-            <input className={styles.container} type="file" id="files" name="files" multiple></input>
-        </>
-    )
-}
--- client/components/form/textarea/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.css';
-
-export default function TextArea() {
-    return (
-        <>
-            <textarea className={styles.container}  rows={4}></textarea>
-        </>
-    )
-}
