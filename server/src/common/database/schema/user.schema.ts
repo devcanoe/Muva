d7247e1e10eb0f6649c12246b978919323a14e2e
neghi14 <sambasseynengimote@gmail.com>
2023-06-07 15:29:30 +0100

    fix: Fixed database schema

--- server/src/common/database/schema/user.schema.ts
+++ server/src/common/database/schema/user.schema.ts
@@ -2,25 +2,31 @@ import { Schema, model } from "mongoose";
 import User from "../model/user.model";
 
 const userSchema: Schema = new Schema<User>({
-  first_name: String,
-  last_name: String,
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     unique: true,
+    required: true,
   },
   password: String,
   phone: Number,
   gender: {
     type: String,
     enum: ["MALE", "FEMALE", "OTHER"],
+    required: true,
   },
   role: {
     type: String,
     enum: ["ADMIN", "GUEST", "USER", "AGENT"],
     default: "GUEST",
   },
-  next_of_kin_name: String,
-  next_of_kin_number: Number,
   created_at: {
     type: Date,
     default: Date.now,
