f5b221ecbae1e7782bcabe90cc74bf3bfcd4eb09
maceteligolden <maceteligolden@gmail.com>
2023-06-09 09:45:55 +0100

    update: sending new code

--- client/src/components/section/pay/index.tsx
+++ client/src/components/section/pay/index.tsx
@@ -4,14 +4,61 @@ import Button from "@/components/form/button";
 import Dropdown from "@/components/form/dropdown";
 import { useRouter } from "next/router";
 import { useAppSelector } from "@/lib/hooks";
-import { ICheckout, selectCurrentCheckout } from "@/lib/slice/tripslice";
+import { IBooking, ICheckout, selectCurrentBooking, selectCurrentCheckout } from "@/lib/slice/tripslice";
+import { useGuestReserveTripMutation } from "@/services/booking.service";
+import { IHandleMotion } from "@/components/displays/toast/toast";
+import { useState } from "react";
 
 export default function PayContent() {
 
     const router = useRouter();
 
-    const checkoutData: ICheckout = useAppSelector(selectCurrentCheckout)
-    console.log(checkoutData)
+    const checkoutData: ICheckout = useAppSelector(selectCurrentCheckout);
+    const bookingData: IBooking = useAppSelector(selectCurrentBooking);
+
+    const [successToastStatus, setSuccessToastStatus] = useState<IHandleMotion>({
+        message: '',
+        visibility: false,
+        status: false
+    });
+    const [errorToastStatus, setErrorToastStatus] = useState<IHandleMotion>({
+        message: '',
+        visibility: false,
+        status: false
+    });
+
+    const successToastHandler = (args: IHandleMotion) => {
+        setSuccessToastStatus(args);
+    }
+
+    const errorToastHandler =(args: IHandleMotion) => {
+        setErrorToastStatus(args);
+    }
+
+    const [ guestReserve, { isLoading } ] = useGuestReserveTripMutation();
+  
+    const makePayment = () => {
+        guestReserve({
+            trip: bookingData.id, 
+            first_name: checkoutData.first_name, 
+            last_name: checkoutData.last_name, 
+            email: checkoutData.email, 
+            phone: checkoutData.phone, 
+            gender: checkoutData.gender, 
+            total_cost: bookingData.cost, 
+            next_of_kin_name: checkoutData.next_of_kin_name, 
+            next_of_kin_number: checkoutData.next_of_kin_phone
+        }).then((res: any) => {
+            router.push(res.data.data.payment.data.authorization_url);
+        }).catch((err: any) => {
+            errorToastHandler({
+                message: err.message,
+                visibility: true,
+                status: false,
+            })
+        })
+    }
+
     return (
         <>
             <section className={styles.container}>
@@ -24,7 +71,7 @@ export default function PayContent() {
                     <p><b>Gender: </b> {checkoutData.gender}</p> 
                     <p><b>Next of kin Name: </b> {checkoutData.next_of_kin_name}</p> 
                     <p><b>Next of kin Phone: </b> {checkoutData.next_of_kin_phone}</p> 
-                    <Button label={"Pay"} onClick={() => {}}/>
+                    <Button isLoading={isLoading} label={"Pay"} onClick={makePayment}/>
                 </div>
             </section>
         </>
--- client/src/services/booking.service.ts
+++ client/src/services/booking.service.ts
@@ -1,6 +1,6 @@
 import { sungloApi } from "../lib/api";
 
-const baseUrl = "trip";
+const baseUrl = "booking";
 
 export interface ISearch {
     departure_location: string;
@@ -12,10 +12,18 @@ export const tripEndpoint = sungloApi.injectEndpoints({
     endpoints: (build: any) => ({
         searchTrip: build.query<any, ISearch>({
             query: (body: any) => `${baseUrl}/all?departure_location=${body.departure_location}&arrival_location=${body.arrival_location}&trip_date=${body.trip_date}`
-        })
+        }),
+        guestReserveTrip: build.mutation({
+            query: (body: any) => ({
+                url: `${baseUrl}/guest-reserve`,
+                method: "POST",
+                body,
+            })
+        }),
     })
 });
 
 export const {
     useSearchTripQuery,
+    useGuestReserveTripMutation,
 } = tripEndpoint;
