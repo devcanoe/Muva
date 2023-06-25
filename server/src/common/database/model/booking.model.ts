6a2040e4d8d6a73641aca82e21d58781fdb85d0c
maceteligolden <maceteligolden@gmail.com>
2023-06-25 16:28:35 +0100

    update: add seat types to booking interface

--- server/src/common/database/model/booking.model.ts
+++ server/src/common/database/model/booking.model.ts
@@ -7,4 +7,7 @@ export default interface Booking extends Base {
   next_of_kin_name?: string;
   next_of_kin_number?: string;
   ref_no?: string;
+  adult: number;
+  child?: number;
+  infant: number;
 }
